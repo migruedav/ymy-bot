2333d1bfb4a6bcb60af3ddd40c9ca191ee93b47c
Miguel Rueda <migruedav@hotmail.com>
2024-03-21 14:10:23 -0600

    Agregado de funcionalidad de bot y actualización de requisitos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # fastapi exameple app
 
 from fastapi import FastAPI
+from api.bot import bot
 
 app = FastAPI()
 
@@ -8,3 +9,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "METDC"}
+
+
+@app.get("/bot")
+async def root():
+    data = bot()
+    return {"message": data}
