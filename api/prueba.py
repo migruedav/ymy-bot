5991bb6ecd3774652d2f236b6881a0ba0a1eecc7
Miguel Rueda <migruedav@hotmail.com>
2024-03-22 12:14:11 -0600

    Actualizar versión del controlador de Chrome

--- api/prueba.py
+++ api/prueba.py
@@ -10,7 +10,9 @@ def prueba():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager(driver_version="2.26").install())
+    )
     driver.get("https://www.youmainlyyou.com/")
 
     return driver.page_source
