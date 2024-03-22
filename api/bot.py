cdbc7c462e61b41ac26163517ccacacc382262c4
Miguel Rueda <migruedav@hotmail.com>
2024-03-21 21:51:19 -0600

    Agregado chromedriver-binary a requirements.txt

--- api/bot.py
+++ api/bot.py
@@ -1,50 +1,51 @@
-import random
-import time
-from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
+    import random
+    import time
+    from seleniumwire import webdriver
+    from selenium.webdriver.chrome.options import Options
+    import chromedriver_binary
 
 
-def bot():
-    try:
-        paises = []
+    def bot():
+        try:
+            paises = []
 
-        for i in range(70):
-            paises.append("mx")
-        for i in range(15):
-            paises.append("us")
-        for i in range(5):
-            paises.append("es")
+            for i in range(70):
+                paises.append("mx")
+            for i in range(15):
+                paises.append("us")
+            for i in range(5):
+                paises.append("es")
 
-        paises.extend(["ar", "co", "pe", "cl", "br", "ec", "gt", "cu", "do", "bo"])
+            paises.extend(["ar", "co", "pe", "cl", "br", "ec", "gt", "cu", "do", "bo"])
 
-        pais = random.choice(paises)
-        print(pais)
-        proxy_options = {
-            "proxy": {
-                "http": f"http://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
-                "https": f"https://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
-                "no_proxy": "localhost,127.0.0.1",
-            },
-            "verify_ssl": False,  # Desactivar la verificación SSL
-            "suppress_connection_errors": False,  # Mostrar errores de conexión
-        }
+            pais = random.choice(paises)
+            print(pais)
+            proxy_options = {
+                "proxy": {
+                    "http": f"http://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                    "https": f"https://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                    "no_proxy": "localhost,127.0.0.1",
+                },
+                "verify_ssl": False,  # Desactivar la verificación SSL
+                "suppress_connection_errors": False,  # Mostrar errores de conexión
+            }
 
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        prefs = {
-            "profile.managed_default_content_settings.images": 2,  # Bloquear imágenes
-            "profile.default_content_setting_values.css": 2,  # Bloquear CSS
-        }
-        options.add_experimental_option("prefs", prefs)
+            options = Options()
+            options.add_argument("--headless")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            prefs = {
+                "profile.managed_default_content_settings.images": 2,  # Bloquear imágenes
+                "profile.default_content_setting_values.css": 2,  # Bloquear CSS
+            }
+            options.add_experimental_option("prefs", prefs)
 
-        driver = webdriver.Chrome(
-            seleniumwire_options=proxy_options, chrome_options=options
-        )
+            driver = webdriver.Chrome(
+                seleniumwire_options=proxy_options, chrome_options=options
+            )
 
-        driver.get("https://www.youmainlyyou.com/")
+            driver.get("https://www.youmainlyyou.com/")
 
         random_time = random.randint(5, 40)
         print("time", random_time)
