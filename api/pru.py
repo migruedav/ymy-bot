47aabbc320364219559ddaea11c1b23f8ff2dff4
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 17:10:54 -0600

    Agregadas opciones de argumento para el modo sin cabeza y sin sandbox en pru.py

--- api/pru.py
+++ api/pru.py
@@ -7,12 +7,12 @@ import time
 def pru():
     try:
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--remote-debugging-port=9222")
-        options.add_argument("--user-data-dir=/root/code/ymy-bot")
+        # options.add_argument("--remote-debugging-port=9222")
+        # options.add_argument("--user-data-dir=/root/code/ymy-bot")
         options.binary_location = "/root/code/ymy-bot"
 
         prefs = {
