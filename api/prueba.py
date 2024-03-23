1d6a37c529af2fa5df6a1762a3afc934cb38b5b1
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 10:04:25 -0600

    Actualiza la función root en main.py y comenta el código en prueba.py

--- api/prueba.py
+++ api/prueba.py
@@ -1,28 +1,28 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
-def prueba():
+# def prueba():
 
-    options = Options()
-    options.add_argument("--headless")  # Run Chrome in headless mode
-    options.add_argument("--no-sandbox")  # Bypass Chrome's sandboxing
-    options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
-    options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource issues
-    options.add_argument("--remote-debugging-port=9222")  # Optional for debugging
+#     options = Options()
+#     options.add_argument("--headless")  # Run Chrome in headless mode
+#     options.add_argument("--no-sandbox")  # Bypass Chrome's sandboxing
+#     options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
+#     options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource issues
+#     options.add_argument("--remote-debugging-port=9222")  # Optional for debugging
 
-    # Ensure the path to ChromeDriver matches your environment
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()), options=options
-    )
+#     # Ensure the path to ChromeDriver matches your environment
+#     driver = webdriver.Chrome(
+#         service=Service(ChromeDriverManager().install()), options=options
+#     )
 
-    print("ya pasó el driver")
+#     print("ya pasó el driver")
 
-    driver.get("https://www.youmainlyyou.com")
-    print(driver.title)
+#     driver.get("https://www.youmainlyyou.com")
+#     print(driver.title)
 
-    driver.quit()
+#     driver.quit()
 
-    return {"ada": "TPEC"}
+#     return {"ada": "TPEC"}
--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 # fastapi exameple app
 
 from fastapi import FastAPI
-from api.bot import bot
-from api.prueba import prueba
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "METCH"}
-
-
-@app.get("/bot")
-async def root():
-    data = bot()
-    return {"message": data}
-
-
-@app.get("/prueba")
-async def root():
-    data = prueba()
-    return "Finished"
+    return {"message": "METC"}
