6dd658b3ff8effbe90db34bb2ed6d8d62cb3a369
Miguel Rueda <migruedav@hotmail.com>
2024-03-22 22:22:59 -0600

    Actualizar ruta de ChromeDriver en prueba.py

--- api/prueba.py
+++ api/prueba.py
@@ -12,7 +12,7 @@ def prueba():
     options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource issues
     options.add_argument("--remote-debugging-port=9222")  # Optional for debugging
 
-    service = Service(executable_path="/ruta/a/tu/chromedriver")
+    service = Service(executable_path="/root")
 
     # Ensure the path to ChromeDriver matches your environment
     driver = webdriver.Chrome(options=options, service=service)
