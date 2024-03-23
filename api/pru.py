3e77b01b3f0da55900790b56ec17acee9634fb70
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 17:09:48 -0600

    Desactivar el modo headless en pru.py

--- api/pru.py
+++ api/pru.py
@@ -7,7 +7,7 @@ import time
 def pru():
     try:
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
