e9fb6a6448f0906992d87f0ba4e69c12ca54d55d
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 17:14:43 -0600

    Actualización de la función de búsqueda para filtrar resultados por fecha

---
+++ api/loc.py
@@ -0,0 +1,85 @@
+from selenium.webdriver.chrome.options import Options
+from seleniumwire import webdriver
+import random
+import time
+
+
+def pru():
+    try:
+        options = Options()
+        # options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+
+        prefs = {
+            "profile.managed_default_content_settings.images": 2,
+            "profile.default_content_setting_values.css": 2,
+        }
+        options.add_experimental_option("prefs", prefs)
+
+        paises = []
+
+        for i in range(70):
+            paises.append("mx")
+        for i in range(15):
+            paises.append("us")
+        for i in range(5):
+            paises.append("es")
+
+        paises.extend(["ar", "co", "pe", "cl", "br", "ec", "gt", "cu", "do", "bo"])
+        pais = random.choice(paises)
+        print(pais)
+
+        proxy_options = {
+            "proxy": {
+                "http": f"http://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                "https": f"https://customer-migruedav-cc-{pais}-sessid-0404864332-sesstime-10:Migruedav1234@pr.oxylabs.io:7777",
+                "no_proxy": "localhost,127.0.0.1",
+            },
+            "verify_ssl": False,
+            "suppress_connection_errors": False,
+        }
+
+        # driver = webdriver.Chrome(options=options, seleniumwire_options=proxy_options)
+        driver = webdriver.Chrome(options=options)
+        driver.get("https://www.youmainlyyou.com/")
+        driver.execute_script("alert('Este es un mensaje de alerta');")
+        time.sleep(5)
+        driver.quit()
+
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time1", random_time)
+        # time.sleep(random_time)
+        # arts = driver.find_elements(by="css selector", value="a:has(img):has(h3)")
+        # cats = [
+        #     "Wellbeing & Health",
+        #     "Allure & Grooming",
+        #     "Taste",
+        #     "Style",
+        #     "Bon Voyage",
+        #     "Awareness",
+        #     "Gadgets",
+        #     "Furnishing",
+        # ]
+        # cat = driver.find_element(by="link text", value=random.choice(cats))
+        # print("cat", cat.text)
+        # cat.click()
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time2", random_time)
+        # time.sleep(random_time)
+        # arts = driver.find_elements(by="css selector", value="a:has(img):has(h3)")
+        # art = random.choice(arts)
+        # art.click()
+        # # random_time = random.randint(1, 10)
+        # random_time = 3
+        # print("time3", random_time)
+        # time.sleep(random_time)
+        # driver.quit()
+
+        return {"pais": pais, "message": "METDC"}
+
+    except Exception as e:
+        return str(e)
