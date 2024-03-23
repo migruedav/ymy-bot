95b2f7de2d129a85bb7e398880d395872be11d25
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 10:46:13 -0600

    Agregadas opciones de configuración para el controlador de Chrome en pru.py

--- api/pru.py
+++ api/pru.py
@@ -6,6 +6,12 @@ def pru():
     try:
         options = Options()
         options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--user-data-dir=/root/code/ymy-bot")
+        options.binary_location = "/root/code/ymy-bot"
 
         driver = webdriver.Chrome(options=options)
         driver.get("https://redis.com/")
