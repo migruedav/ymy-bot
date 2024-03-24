56adfdb34f0d880acc9128cf33f5f55863e8aec5
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 18:48:20 -0600

    Actualizada la ubicación binaria de Google Chrome en pru.py

--- api/pru.py
+++ api/pru.py
@@ -12,7 +12,7 @@ def pru():
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--disable-dev-shm-usage")
-        options.binary_location = "/usr/bin/google-chrome"
+        options.binary_location = "/root/code/ymy-bot"
 
         prefs = {
             "profile.managed_default_content_settings.images": 2,
